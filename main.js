a4f19757c57104358905e0e2555974e92901f418
veprogames <veprogames.gamedev@gmail.com>
2023-01-05 21:32:29 +0100

    layouting and background

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
+const loadingText = document.getElementById("loading");
 
 let images = {};
 
 const rand = obj => Object.values(obj)[Math.floor(Math.random() * Object.values(obj).length)];
+const BPM = 172;
 
 function draw(){
     ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -11,7 +13,12 @@ function draw(){
     ctx.drawImage(rand(images.mouths), 0, 0, canvas.width, canvas.height);
 }
 
+canvas.style.display = "none";
+
 getAllImages().then(img => {
+    loadingText.style.display = "none";
+    canvas.style.display = "block";
     images = img;
-    setInterval(draw, 420);
+    draw();
+    setInterval(draw, 60000 / BPM);
 });
